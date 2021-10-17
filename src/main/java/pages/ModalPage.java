ceeea6826092a5425319094aee8109afce7ff098
CptWasp <StigNelson11@gmail.com>
2021-10-17 22:39:26 +0300

    task 3, advanced search continuation + bug fixed

--- src/main/java/pages/ModalPage.java
+++ src/main/java/pages/ModalPage.java
@@ -9,24 +9,24 @@ public class ModalPage {
 
 
     public void selectTrueCheckBoxes(){
-        $(By.xpath("//label[text()='Искать в файлах']/parent::*/input")).setSelected(false);
-        $(By.xpath("//label[text()='Точное соответствие']/parent::*/input")).setSelected(false);
-        $(By.xpath("//label[text()='Исключить совместные закупки']/parent::*/input")).setSelected(true);
-        $(By.xpath("//label[text()='Только МСП / СМП']/parent::*/input")).setSelected(false);
+//        $(By.xpath("//label[text()='Искать в файлах']/parent::*/input")).setSelected(false);
+//        $(By.xpath("//label[text()='Точное соответствие']/parent::*/input")).setSelected(false);
+//        $(By.xpath("//label[text()='Исключить совместные закупки']/parent::*/input")).setSelected(true);
+//        $(By.xpath("//label[text()='Только МСП / СМП']/parent::*/input")).setSelected(false);
 
 
-//        if($(By.xpath("//label[text()='Искать в файлах']/parent::*/input")).isSelected()){
-//            $(By.xpath("//label[text()='Искать в файлах']/parent::*/input")).click();
-//        }
-//        if ($(By.xpath("//label[text()='Точное соответствие']/parent::*/input")).isSelected()){
-//            $(By.xpath("//label[text()='Точное соответствие']/parent::*/input")).click();
-//        }
-//        if(!$(By.xpath("//label[text()='Исключить совместные закупки']/parent::*/input")).isSelected()){
-//            $(By.xpath("//label[text()='Исключить совместные закупки']/parent::*/input")).click();
-//        }
-//        if($(By.xpath("//label[text()='Только МСП / СМП']/parent::*/input")).isSelected()){
-//            $(By.xpath("//label[text()='Только МСП / СМП']/parent::*/input")).click();
-//        }
+        if($(By.xpath("//label[text()='Искать в файлах']/parent::*/input")).isSelected()){
+            $(By.xpath("//label[text()='Искать в файлах']")).click();
+        }
+        if ($(By.xpath("//label[text()='Точное соответствие']/parent::*/input")).isSelected()){
+            $(By.xpath("//label[text()='Точное соответствие']")).click();
+        }
+        if(!$(By.xpath("//label[text()='Исключить совместные закупки']/parent::*/input")).isSelected()){
+            $(By.xpath("//label[text()='Исключить совместные закупки']")).click();
+        }
+        if($(By.xpath("//label[text()='Только МСП / СМП']/parent::*/input")).isSelected()){
+            $(By.xpath("//label[text()='Только МСП / СМП']")).click();
+        }
 
 
 
