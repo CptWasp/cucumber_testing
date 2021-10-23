ac6e548857467879a8732eff58bffb951e9bd7b4
CptWasp <StigNelson11@gmail.com>
2021-10-23 06:28:11 +0300

    build + add main class

--- src/test/java/RunCucunberTest.java
+++ src/test/java/RunCucunberTest.java
@@ -5,4 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = "pretty", features = "src/test/resources/features")
 public class RunCucunberTest {
+    public static void main(String[] args){
+        cucumber.api.cli.Main.main(args);
+    }
 }
